d161a79a9ea8476e0aa9c60cb97797172bb6b180
rere <regis-graptin@sfr.fr>
2023-07-22 14:43:59 +0200

    change token name

--- polygon_id/smartcontract_verify/contracts/ERC20Verifier.sol
+++ polygon_id/smartcontract_verify/contracts/ERC20Verifier.sol
@@ -13,10 +13,21 @@ contract ERC20Verifier is ERC20, ZKPVerifier {
     mapping(address => uint256) public addressToId;
 
     uint256 public TOKEN_AMOUNT_PER_ID = 1;
+    uint256 public increments = 0;
 
     constructor(string memory name_, string memory symbol_)
     ERC20(name_, symbol_)
     {}
+
+    function test_1() public returns(uint256) {
+        increments += 1;
+        return increments;
+    }
+
+    function test_2() public returns(uint256) {
+        increments += 2;
+        return increments;
+    }
 
     function _beforeProofSubmit(
         uint64, /* requestId */
